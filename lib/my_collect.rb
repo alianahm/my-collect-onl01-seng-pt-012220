eaaef8f14ddf6145cdd9e6b8efa414c7879b7806
Alianah Marie Magalued <alianahmagalued@yahoo.com>
2020-02-01 06:32:39 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,9 +1,10 @@
-def my_collect(array)
+def each(array)
 new_array = []
  i = 0
   while i < array.length
-      new_array << yield array[i]
-      #new_array.push(yield array[i])
+      #new_array << yield array[i]
+      new_array.push(yield array[i])
       i += 1
   end
+  new_array
 end
